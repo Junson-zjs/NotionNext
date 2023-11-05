1fbd6ce49e17904ec9ec8ef6b01ef97919835536
Junson-zjs <1097437942@qq.com>
2023-11-05 13:10:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '打破无明见真实', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '带你看清真实的世界', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.tianshanyuan.net', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '修行' // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '修行', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
